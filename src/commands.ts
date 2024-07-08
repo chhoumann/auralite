87b499eebf53bc369eb422540cd02715f5e81be5
Christian Bager Bach Houmann <christian@bagerbach.com>
2024-07-08 10:29:07 +0200

    refactor: move commands to separate file

---
+++ src/commands.ts
@@ -0,0 +1,29 @@
+import { Notice, type Command } from "obsidian";
+import type OVSPlugin from "./main";
+
+declare const __IS_DEV__: boolean;
+
+export function registerCommands(plugin: OVSPlugin): void {
+	const commands: Array<Command> = [
+		{
+			id: "reload-ovs",
+			name: `Reload ${plugin.manifest.name}${__IS_DEV__ ? " (dev)" : ""}`,
+			checkCallback: (checking: boolean) => {
+				if (checking) {
+					return __IS_DEV__;
+				}
+				if (!checking && __IS_DEV__) {
+					(async () => {
+						await plugin.app.plugins.disablePlugin(plugin.manifest.id);
+						await plugin.app.plugins.enablePlugin(plugin.manifest.id);
+						new Notice("Plugin reloaded");
+					})();
+				}
+			},
+		},
+	];
+
+	for (const cmd of commands) {
+		plugin.addCommand(cmd);
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
-import { Notice, Plugin } from "obsidian";
-
-declare const __IS_DEV__: boolean;
+import { Plugin } from "obsidian";
+import { registerCommands } from "./commands";
 
 export default class OVSPlugin extends Plugin {
 	override async onload() {
-		this.addCommand({
-			id: "reload-ovs",
-			name: "Reload OVS",
-			checkCallback: (checking: boolean) => {
-				if (checking) {
-					return __IS_DEV__;
-				}
-				if (!checking && __IS_DEV__) {
-					(async () => {
-						await this.app.plugins.disablePlugin(this.manifest.id);
-						await this.app.plugins.enablePlugin(this.manifest.id);
-						new Notice("Plugin reloaded");
-					})();
-				}
-			},
+		registerCommands(this);
+
+		this.addRibbonIcon("mic", "Start chat", () => {
+			console.log("Start chat");
 		});
 	}
 
